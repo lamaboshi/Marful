6c5e8e2c9f3a8fd3e4f9066c57fb2796d9a44ca9
asiabadnjki <assiabadnjki@gmail.com>
2022-06-12 21:46:21 +0300

    password

--- lib/app/modules/password_page/views/confirm_password.dart
+++ lib/app/modules/password_page/views/confirm_password.dart
@@ -1,64 +1,147 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:marful/app/modules/menu/views/menu_view.dart';
+import 'package:marful/app/modules/websit_company/views/websit_company_page.dart';
 
+import '../../../core/values/app_colors.dart';
 import '../../../routes/app_pages.dart';
 
 class Confirmpassword extends StatelessWidget{
   const Confirmpassword({super.key});
   @override
   Widget build (BuildContext context){
-
+ var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
+      
     body: Center(child: Column(children: [
       Row(children: [
          Padding(
                      padding: const EdgeInsets.all(8.0),
                      child: IconButton(onPressed: (){
-                       Navigator.pop(context);
+                          Get.toNamed(Routes.Password);
                      }, 
                      icon:Icon(Icons.arrow_back,size: 30,
-                     color: Colors.purple[900],) ),
+                     color: AppColors.blue) ),
                    ),
           Padding(
                      padding: const EdgeInsets.fromLTRB(12,0,0,0),
                      child: Text('Reset  Your Password' ,style: TextStyle(fontSize: 22
-                     ,color: Colors.purple[900],), ),
+                     ,color:AppColors.blue), ),
                    ),
       ],),
-        Card(elevation: 10,child: Container( width: 180,
-       
-        child: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6rpTLf6jrGAxcMuh5O3pmbO6cBmZlXkY4Cw&usqp=CAU'),)),
+        Card(elevation: 10,child: Container( width: width/2,height: height/2,
+        child: Image.asset('assets/images/comf.gif'))),
          Padding(
            padding: const EdgeInsets.all(8.0),
-           child: Text('Enter your new password below',style: TextStyle(fontSize: 20),),
+           child: Text('Enter your new password below',style: TextStyle(fontSize: 20,color: Colors.black54),),
          )   ,
-          Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: Container( width:400,
-                     child: TextField( decoration: InputDecoration(border:OutlineInputBorder(),
-                     hintText: 'Enter New Password' ,labelText: 'New Password'),),
-                   ),
-                 ),
-                   Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: Container( width:400,
-                     child: TextField( decoration: InputDecoration(border:OutlineInputBorder(),
-                     hintText: 'Enter Your Confrim password' ,labelText: 'Confrim password'),),
-                   ),
-                 ),
-                   Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: ElevatedButton(
-                     style:ElevatedButton.styleFrom(primary: Colors.pink,minimumSize: 
-                     Size(405, 60),
-                     shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(5))
-                     ),
-                     onPressed: (){
-                       Get.toNamed(Routes.websitecompany);
-                      // Navigator.push(context, MaterialPageRoute(builder: (context) => const secondpage(),));
-                     }, child:Text('SUBMIT',style: TextStyle(fontSize: 20),) ),
-                   ),
+          // Padding(
+          //          padding: const EdgeInsets.all(8.0),
+          //          child: Container( width:400,
+          //            child: TextField( decoration: InputDecoration(border:OutlineInputBorder(),
+          //            hintText: 'Enter New Password' ,labelText: 'New Password'),),
+          //          ),
+          //        ),
+                //    Padding(
+                //    padding: const EdgeInsets.all(8.0),
+                //    child: Container( width:400,
+                //      child: TextField( decoration: InputDecoration(border:OutlineInputBorder(),
+                //      hintText: 'Enter Your Confrim password' ,labelText: 'Confrim password'),),
+                //    ),
+                //  ),
+                 SizedBox(height: 10,),
+                   const TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        cursorColor: AppColors.blue,
+                        cursorHeight: 20,
+                        autofocus: false,
+                        decoration: InputDecoration(
+                            labelStyle: TextStyle(color: Colors.grey),
+                            labelText: ' Password',
+                            hintText: "New password",
+                            prefixIcon: Icon(
+                              Icons.lock_open_outlined,
+                              color: AppColors.orange,
+                            ),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.orange, width: 1.5),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 15),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.orange, width: 1.5),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.orange, width: 1.5),
+                            )),
+                      ),
+                      SizedBox(height: 10,),
+                         const TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        cursorColor: AppColors.blue,
+                        cursorHeight: 20,
+                        autofocus: false,
+                        decoration: InputDecoration(
+                            labelStyle: TextStyle(color: Colors.grey),
+                            labelText: 'Confirm',
+                            hintText: "Comfirmpassword",
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: AppColors.orange,
+                            ),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.orange, width: 1.5),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 15),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.orange, width: 1.5),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.orange, width: 1.5),
+                            )),
+                      ),
+                 
+                  //  Padding(
+                  //    padding: const EdgeInsets.all(8.0),
+                  //    child: ElevatedButton(
+                  //    style:ElevatedButton.styleFrom(primary:  AppColors.blue,minimumSize: 
+                  //    Size(405, 60),
+                  //    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(5))
+                  //    ),
+                  //    onPressed: (){
+                  //      Get.toNamed(Routes.websitecompany);
+                  //     // Navigator.push(context, MaterialPageRoute(builder: (context) => const secondpage(),));
+                  //    }, child:Text('SUBMIT',style: TextStyle(fontSize: 20),) ),
+                  //  ),
+                    SizedBox(height: 20,),
+                  ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(AppColors.blue),
+                            fixedSize: MaterialStateProperty.all(
+                                const Size.fromWidth(150))),
+                        onPressed: () {
+                Get.toNamed(Routes.websitecompany);
+                        },
+                        child: const Text(
+                          "SUBMIT",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
     ],)
 
     ),
