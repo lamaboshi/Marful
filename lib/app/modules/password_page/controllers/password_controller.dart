e180319db13d1a5edf0671b14420a0f9945eb74d
asiabadnjki <assiabadnjki@gmail.com>
2022-06-30 21:09:49 +0300

    password-new

--- lib/app/modules/password_page/controllers/password_controller.dart
+++ lib/app/modules/password_page/controllers/password_controller.dart
@@ -1,47 +1,61 @@
 import 'dart:html';
 
 import 'package:get/get.dart';
+import 'package:marful/app/data/model/company.dart';
 import 'package:marful/app/data/model/infulonser.dart';
+import 'package:marful/app/data/model/user_model.dart';
 
+import '../../../../sheard/auth_service.dart';
+import '../../../routes/app_pages.dart';
 import '../data/adapter/password_adapter.dart';
 import '../data/password_repository.dart';
-
 class PasswordController extends GetxController {
   var allEmails=<Infulonser>[].obs;
-  var infuloncer =Infulonser(id: 0, name: '', phone: 'phone', email:' email', paypal: 'paypal', address: 'address', password: 'password', userName: 'userName', image: null, description: 'description');
-  final passRepo= PasswordRepository();
-  final entrvalue=''.obs;
+  final Repo= PasswordRepository();
+  final resrtpassword=''.obs;
  final email=''.obs;
-  final result=false.obs;
+  final auth=Get.find<AuthService >();
+    final typeAuth=Auth.user.obs;
+  //final result=false.obs;
+  final company=Company().obs;
+  final user=UserModel().obs;
+  final  infulencer=Infulonser().obs;
+final type=''.obs;
   
   @override
   void onInit() {
     super.onInit();
-  getAllEmails();
-  Result();
   }
-    Future<void> Result() async {
-   Infulonser i=allEmails.firstWhere((element) => element.email==email);
-    if(i!=null){
-      result.value=true;
-    }
-    
+  void getEmail(){
+  var data=Repo.getEmail(email.value);
+  if(data!=null){
+  type.value=data as String;
+  Get.rootDelegate.offNamed(Routes.ConfirmPassword);
   }
-  
-    Future<void> getAllEmails() async {
-    var data = await passRepo.getdata();
-    allEmails.assignAll(data);
-  }
-   Future<void> Resetpassword(String email) async {
-  infuloncer.email=email;
-     passRepo. updatedata( infuloncer);
- 
-    //   await _auth
-    //     .sendPasswordResetEmail(email: email)
-    //     .then((value) => _status = AuthStatus.successful)
-    //     .catchError((e) => _status = AuthExceptionHandler.handleAuthException(e));
-    // return _status;
+   void resetPassword(){
+          switch (type.value
+           // auth.personType()
+            ) {
+        case 'user':
+          typeAuth.value=Auth.user;
+          user.value=auth.getDataFromStorage() as UserModel;
+          user.value.password=resrtpassword.value;
+          Repo.resetPassUser(user.value);
+          break;
+        case 'comapny':
+              typeAuth.value=Auth.comapny;
+          company.value=auth.getDataFromStorage() as Company;
+            company.value.password=resrtpassword.value;
+           // Repo.resetPassComp(company.value);
+          break;
+        case 'infulonser':
+             typeAuth.value=Auth.infulonser;
+          infulencer.value=auth.getDataFromStorage() as Infulonser;
+          infulencer.value.password=resrtpassword.value;
+          Repo.resetPassInful(infulencer.value);
+          break;
+      }
+   }
+  }  
   }
 
-
-}
