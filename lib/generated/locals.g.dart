eb8cbd7e6ee6bc567d8985ba246749c41f8afcd5
lamaboshi <lamaboshi.99@gmail.com>
2022-08-09 13:57:20 +0300

    Local

---
+++ lib/generated/locals.g.dart
@@ -0,0 +1,8 @@
+// Code generated at 2022-07-06 17:31:45.803509 by Qlevar Local Manager
+
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    "en_EN": {"hi": "hi"},
+    "ar_AR": {"hi": "hi"},
+  };
+}
